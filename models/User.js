d93a4cfd8922f1e4df1963a9d242307f59faf7d3
Jackey <jackey.li@foxmail.com>
2015-05-16 11:40:26 +0800

    remove useless code

--- models/User.js
+++ models/User.js
@@ -1,9 +1,7 @@
 /**
  * Created by Jackey Li on 2015/5/15.
  */
-/**
- * Created by Administrator on 2015/4/22.
- */
+
 var mongodb = require('./db'),
     BSON = require('mongodb').BSONPure;
 
--- settings.js
+++ settings.js
@@ -1,9 +1,7 @@
 /**
  * Created by Jackey Li on 2015/5/15.
  */
-/**
- * Created by Administrator on 2015/4/22.
- */
+
 module.exports = {
     cookieSecret: 'lunch_Order',
     db: 'lunchOrder',
